2da013306b2f317bf5031c7f4421e7de80126858
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-06 19:45:51 -0400

    added notes on doubles.

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ int main (){
 
     // age will now behave as 25
 
+    printf("You are %d years old" , age);
+    printf("\nThe year is %d" , year);
+
     float gpa = 2.5;
     float price = 19.99; 
     //^ used for decimal values 
@@ -32,9 +35,13 @@ int main (){
     printf("The price is %.2f\n", price);
     //^ this would be the formatting for displaying a flot with two decimal values.
 
-    printf("You are %d years old" , age);
-    printf("\nThe year is %d" , year);
-
+    double pi = 3.14159265358979;
+    // if you need more precision you can utilize a double 
+    printf("The value of pi is %.15lf", pi);
+    // %lf will be the formatter for doubles (lf - long float)
+    // the default behavior of c will display this as only 6 characters after
+    // the decimal you can use precision in order to calculate the rest of the
+    // characters (%.15lf in this case)
     return 0;
     // ^ return 0; is used because the function is expected to return an integer 
     // to the OS. The number indicates a code. 0 means successfull. 
