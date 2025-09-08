a73b7bb806157c6ed7f1d49dc9c5c59a4debfb24
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-08 09:04:45 -0400

    notes on user input

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 //main function needed in order for a program to be used or ran.
 int main (){
 
-    printf("I like Pizza!\n");
-    printf("Its really good!");
+    //printf("I like Pizza!\n");
+    //printf("Its really good!");
     //^ this function allows you to print script to the screen.
     // \n adds a new line - placed inside of ""
 
@@ -25,65 +25,65 @@ int main (){
     // char[] - array of characters (size varies)
     // bool - true or false (1 byte and requires <stdbool.h>)
 
-    int age = 25;
+    //int age = 25;
     // int are whole numbers
-    int year = 2025;
+    //int year = 2025;
 
     // age will now behave as 25
 
-    printf("You are %d years old" , age);
-    printf("\nThe year is %d" , year);
+   // printf("You are %d years old" , age);
+    //printf("\nThe year is %d" , year);
 
-    float gpa = 2.5;
-    float price = 19.99; 
+    //float gpa = 2.5;
+    //float price = 19.99; 
     //^ used for decimal values 
-    printf("\nyour gpa is %f\n", gpa);
+    //printf("\nyour gpa is %f\n", gpa);
     //^ then formatting a float use %f 
     //^ when working with floating point numbers C will print out 6 places
     // the decimal so 2.500000 will print in this case. you can alter the amount of 
     //deciaml places being shown by placing a number in front of the formatter like 
     // %.1f
-    printf("The price is %.2f\n", price);
+    //printf("The price is %.2f\n", price);
     //^ this would be the formatting for displaying a flot with two decimal values.
 
-    double pi = 3.14159265358979;
+    //double pi = 3.14159265358979;
     // if you need more precision you can utilize a double 
-    printf("The value of pi is %.15lf\n", pi);
+    //printf("The value of pi is %.15lf\n", pi);
     // %lf will be the formatter for doubles (lf - long float)
     // the default behavior of c will display this as only 6 characters after
     // the decimal you can use precision in order to calculate the rest of the
     // characters (%.15lf in this case)
 
-    char grade = 'A';
-    char symbol = '!';
+    //char grade = 'A';
+    //char symbol = '!';
     // when using char you need to utlize sigle ''
-    printf("Your grade is %c\n", grade);
-    printf("Your favorite symbol is %c\n", symbol);
+   //printf("Your grade is %c\n", grade);
+    //printf("Your favorite symbol is %c\n", symbol);
     // formatting for character requires the %c (c for character)
 
-    char name[] = "Jesus Gonzalez";
-    char food[] = "Pizza";
-    char email[] = "jesusgonzalez0516@gmail.com";
+    //char name[] = "Jesus Gonzalez";
+    //char food[] = "Pizza";
+    //char email[] = "jesusgonzalez0516@gmail.com";
     // in C language in order to store a string you need to utilize an array
     // for the characters as shown above. 
     // you can store more than one char but they have to be in "" 
 
-    printf("Hello %s\n", name);
-    printf("My favorite food is %s\n", food);
-    printf("Your email. is %s\n", email);
+   // printf("Hello %s\n", name);
+    //printf("My favorite food is %s\n", food);
+    //printf("Your email. is %s\n", email);
     // formatted using the %s (s for string)
 
-    bool isOnline = true;
+   // bool isOnline = true;
     // true = 1, false = 0
     // boolean are good for if statements 
-    printf("%d\n", isOnline);
+    //printf("%d\n", isOnline);
 
-    if(isOnline){
+    /*if(isOnline){
         printf("You are online\n");
     } else {
         printf("You are offline\n");
     };
-
+    */
 //////////////
 
     // Format specifiers = special tokens that begin with % symbol
@@ -100,7 +100,7 @@ int main (){
     //     this is a good way at testing to see if a number is even or odd.
     // ++ - incriment 
     // -- - decriment 
-
+    /*
     int x = 2;
     int y = 3;
     int z = 0;
@@ -109,7 +109,32 @@ int main (){
 
     // x+=3 = x = x +3 
 
-    printf("%d", z);
+    printf("%d\n", z);
+    */
+   ////////
+    // User input 
+
+    int age = 0;
+    float gpa = 0.0f; 
+    char grade = '\0';
+    //null character use to clear out 
+    char name[30] = "";
+    // name has max size of 30 char  
+
+    printf("Hello! What is your name?");
+    scanf("%s", &name);
+
+    printf("Enter Your Age ");
+    scanf("%d", &age);
+
+    printf("Enter Your Gpa ");
+    scanf("%f", &gpa);
+
+    printf("Hello! %s!\n", name);
+    printf("You are %d years old!\n", age);
+    printf("You have a gpa of: %.2f\n", gpa);
+    printf("%c\n", grade);
+
 
 
 
